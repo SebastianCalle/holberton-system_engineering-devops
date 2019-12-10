6c18ae3687d6438a20f64ace0c1e79afe4a45f32
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-12-10 23:33:14 +0000

    [feature]: create a manifest to change ssh_config file

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # configurate ssh_config
-exec { 'auth':
-  path    => '/bin'
+exec { 'ssh_config':
+  path    => '/bin',
   command => 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/holberton" > /etc/ssh/ssh_config'
 }
