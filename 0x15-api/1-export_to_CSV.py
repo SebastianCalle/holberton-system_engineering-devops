9cece59fa97f979c27f2d9f22e542b19e8da2119
Juan Sebastian Montealegre <jusemonca@gmail.com>
2020-02-03 21:06:02 +0000

    [Feature]: Create export to csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Api REST JSONPlaceholder
+"""
+import requests
+import sys
+import csv
+
+if __name__ == '__main__':
+    employee_id = sys.argv[1]
+    res = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
+    res2 = requests.get(
+        'https://jsonplaceholder.typicode.com/todos'.format(employee_id))
+    username = res.json().get('username')
+    list_emp = res2.json()
+    list_task = []
+    total_task = 0
+    for emp in list_emp:
+        if emp.get('userId') == int(employee_id):
+            list_task.append(emp)
+    with open('{}.csv'.format(employee_id), mode='w') as employee_file:
+        employee_writer = csv.writer(employee_file, delimiter=',')
+        for em in list_task:
+            employee_writer.writerow([em['userId'], username, em['completed'],
+                                      em['title']])
