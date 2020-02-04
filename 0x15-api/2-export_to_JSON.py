461cdce0be6bfe2fa2156cf7391642a59992e729
Juan Sebastian Calle <jusemonca@gmail.com>
2020-02-03 21:21:20 -0500

    [Feature]: Export a json file

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     employee_id = 0
     i = 1
     dict_emp = {}
-    while (i <= 1):
+    while (i <= 10):
         res = requests.get(
             'https://jsonplaceholder.typicode.com/users/{}'.format(i))
         username = res.json().get('username')
@@ -22,19 +22,13 @@ if __name__ == '__main__':
         for emp in list_emp:
             if i == emp.get('userId'):
                 new_dict[emp['id']] = {
-                    "completed": emp['completed'],
                     "task": emp['title'],
+                    "completed": emp['completed'],
                     "username": username
                 }
-                list_users.append(new_dict.values())
+                list_users.append(new_dict.get(emp['id']))
         dict_emp[i] = list_users
         i += 1
 
-    print(dict_emp)
-
-#    with open('todo_all_employes.csv', mode='w') as employee_file:
-#        employee_writer = csv.writer(employee_file, delimiter=',',
-#                                     quoting=csv.QUOTE_ALL)
-#        for em in list_task:
-#            employee_writer.writerow([em['userId'], username, em['completed'],
-#                                      em['title']])
+    with open('todo_all_employes.csv', mode='w') as employee_file:
+        json.dump(dict_emp, employee_file)
