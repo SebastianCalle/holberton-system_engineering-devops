4bb2d7a4c6c0f40e2d70a5929ced25b9ed90ba2f
Juan Sebastian Montealegre <jusemonca@gmail.com>
2020-02-06 17:19:19 +0000

    [Feature]: Create README and function that return the number of subscribers of api reddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,11 +13,9 @@ def number_of_subscribers(subreddit):
     headers = {'user-agent': '/u/Api advance project'}
     client = requests.session()
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    res = client.get(url, headers=headers)
-    data = None
+    res = client.get(url, headers=headers, allow_redirects=False)
     if res.status_code == 200:
         data = res.json().get('data').get('subscribers')
-    if data:
         return data
     else:
         return 0
