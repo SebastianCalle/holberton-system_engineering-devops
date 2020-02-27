a080ec381df8ce2ad4b74b493447b891f1bb2a70
Juan Sebastian Calle <jusemonca@gmail.com>
2020-02-27 11:34:32 -0500

    [Feature]: add the restart nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,6 @@
 # Debug nginx
 exec { 'fix-nginx':
-  command => "sed -i 's/-n 15/-n 4096' /etc/default/nginx",
+  command => "sed -i 's/-n 15/-n 4096' /etc/default/nginx\
+  service nginx stop; service nginx start",
   path    => '/usr/bin:/usr/sbin:/bin'
 }
